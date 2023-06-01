50c74b723711ce1495965c7f63b17a486626dd91
nhoxlong020220 <nhoxlong020220@gmail.com>
2023-06-01 21:59:08 +0700

    upadte

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const cors = require('cors');
 const { Db, MongoDBNamespace } = require("mongodb")
 const userRoute = require("./route/users")
 const authRoute = require("./route/auth")
@@ -38,12 +39,26 @@ app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/posts", postRoute)
 app.use("/api/comments", commentRoute)
-// app.get("/", (req, res) => {
-//   res.send("Hello homepage")
-// })
-// app.get("/users", (req, res) => {
-//   res.send("Hello userpage")
-// })
+app.use(function (req, res, next) {
+  // Set the Access-Control-Allow-Origin header to "" to allow requests from any origin.
+  res.header('Access-Control-Allow-Origin', '');
+
+  // Set the Access-Control-Allow-Headers header to allow the Content-Type header.
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Set the Access-Control-Allow-Methods header to allow the GET, POST, PUT, DELETE, and OPTIONS methods.
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  // Set the Access-Control-Max-Age header to 1 hour to cache the CORS response.
+  res.header('Access-Control-Max-Age', 3600);
+
+  next();
+});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 
 app.listen(1812, () => {
   console.log("Backend is running")
