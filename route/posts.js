4baf32dd27a058972b3ef0ca165c3621b8b89002
nhoxlong020220 <nhoxlong020220@gmail.com>
2023-06-01 22:00:25 +0700

    update post

--- route/posts.js
+++ route/posts.js
@@ -12,9 +12,9 @@ router.post("/", async (req, res) => {
     const newPost = new Post(req.body)
     try {
         const savedPost = await newPost.save()
-        res.status(200).json(savedPost)
+        return res.status(200).json(savedPost)
     } catch (err) {
-        res.status(500).json(err)
+        return res.json("Error !")
     }
 })
 // update a post
@@ -24,12 +24,12 @@ router.put("/:id", async (req, res) => {
         const post = await Post.findById(req.params.id)
         if (post.userId === req.body.userId) {
             await post.updateOne({ $set: req.body })
-            res.status(200).json("Post have been update")
+            return res.status(200).json("Post have been update")
         } else {
-            res.status(403).json("You can update only your post")
+            return res.status(403).json("You can update only your post")
         }
     } catch (err) {
-        res.status(500).json(err)
+        return res.json("Error !")
     }
 })
 // delete a post
@@ -39,12 +39,12 @@ router.delete("/:id", async (req, res) => {
         const post = await Post.findById(req.params.id)
         if (post.userId === req.body.userId) {
             await post.deleteOne()
-            res.status(200).json("Post have been deleted")
+            return res.status(200).json("Post have been deleted")
         } else {
-            res.status(403).json("You can delete only your post")
+            return res.status(403).json("You can delete only your post")
         }
     } catch (err) {
-        res.status(500).json(err)
+        return res.json("Error !")
     }
 })
 // like and dislike a post
@@ -54,13 +54,13 @@ router.put("/:id/like", async (req, res) => {
         const post = await Post.findById(req.params.id)
         if (!post.likes.includes(req.body.userId)) {
             await post.updateOne({ $push: { likes: req.body.userId } })
-            res.status(200).json("Post have been liked")
+            return res.status(200).json("Post have been liked")
         } else {
             await post.updateOne({ $pull: { likes: req.body.userId } })
-            res.status(200).json("Post have been disliked")
+            return res.status(200).json("Post have been disliked")
         }
     } catch (err) {
-        res.status(500).json(err)
+        return res.json("Error !")
     }
 })
 
@@ -68,9 +68,9 @@ router.put("/:id/like", async (req, res) => {
 router.get("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
-        res.status(200).json(post)
+        return res.status(200).json(post)
     } catch (err) {
-        return res.status(500).json(err)
+        return res.json("Error !")
     }
 })
 // get all likes
@@ -85,9 +85,9 @@ router.get("/all/likes", async (req, res) => {
             let userNameLike = user.username
             userLikes.push(userNameLike)
         }
-        res.status(200).json(userLikes)
+        return res.status(200).json(userLikes)
     } catch (err) {
-        return res.status(500).json(err)
+        return res.json("Error !")
     }
 })
 
@@ -102,9 +102,9 @@ router.get("/timeline/all", async (req, res) => {
                 return Post.find({ userId: friendId })
             })
         )
-        res.json(userPost.concat(...friendPost))
+        return res.json(userPost.concat(...friendPost))
     } catch (err) {
-        return res.status(500).json(err)
+        return res.json("Error !")
     }
 })
 
