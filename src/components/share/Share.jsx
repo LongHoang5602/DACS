aa5dcd1a4577f7dd0b4db514db3fe7f31edeba25
thanhlong <longlieulinh111@gmail.com>
2023-06-13 23:32:12 +0700

    update share

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -243,84 +243,259 @@
 
 
 //tesst4
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useSessionStorage } from "react-use";
-import "./share.scss";
-import Image from "../../assets/img.png";
-import Map from "../../assets/map.png";
-import Friend from "../../assets/friend.png";
-import { useContext } from "react";
-import { AuthContext } from "../../context/authContext";
+// import React, { useState } from 'react'
+// import axios from 'axios'
+// import { useSessionStorage } from "react-use";
+// import "./share.scss";
+// import Image from "../../assets/img.png";
+// import Map from "../../assets/map.png";
+// import Friend from "../../assets/friend.png";
+// import { useContext } from "react";
+// import { AuthContext } from "../../context/authContext";
+
+// const Share = () => {
+//     const fs = require('fs');
+
+
+
+//     const { user } = useContext(AuthContext);
+//     const currentUser = user.currentUser;
+
+
+//     const [desc, setDesc] = useState('')
+//     const [selectedFile, setSelectedFile] = useState(null)
+
+//     const [token, setToken] = useSessionStorage("token");
+//     const handleInputChange = (e) => {
+//         setDesc(e.target.value)
+//     }
+
+//     const handleFileChange = (e) => {
+//         setSelectedFile(e.target.files[0])
+//     }
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+
+//         const data = new FormData();
+//         const fileStream = fs.createReadStream('/path/to/file.jpg');
+//         data.append('img', fileStream);
+//         data.append('decs', 'alo alo');
+
+//         const config = {
+//             method: 'post',
+//             maxBodyLength: Infinity,
+//             url: 'http://localhost:1812/api/posts',
+//             headers: {
+//                 Authorization: `Bearer ${token}`,
+//                 Cookie: `refreshToken=${document.cookie}`,
+//                 ...data.getHeaders()
+//             },
+//             data: data,
+//         };
+
+//         axios(config)
+//             .then(response => {
+//                 console.log(response.data);
+//             })
+//             .catch(error => {
+//                 console.error(error);
+//             });
+//     }
+
+//     // const handleSubmit = async (e) => {
+//     //     e.preventDefault()
+//     //     const data = new FormData()
+//     //     data.append('desc', desc)
+//     //     data.append('image', selectedFile)
+
+//     //     // try {
+//     //     //     const res = await axios.post('http://localhost:1812/api/posts', data, {
+//     //     //         headers: {
+//     //     //             Authorization: `Bearer ${token}`,
+//     //     //             'Content-Type': 'multipart/form-data'
+//     //     //         }
+//     //     //     })
+//     //     //     alert(res.data)
+//     //     // } catch (err) {
+//     //     //     alert(err.response.data)
+//     //     // }
+//     //     const makePostRequest = async (data) => {
+//     //         const headers = {
+//     //             Authorization: `Bearer ${document.cookie}`,
+//     //             Cookie = refreshToken=document.cookies,
+//     //             //'Content-Type': 'multipart/form-data'
+//     //         };
+
+//     //         try {
+//     //             const res = await axios.post('http://localhost:1812/api/posts', data, headers);
+//     //             alert(res.data);
+//     //         } catch (err) {
+//     //             alert(err.response.data);
+//     //         }
+//     //     };
+//     //     await makePostRequest(data);
+//     // }
+
+//     return (
+//         <div className="share">
+//             <div className="container">
+//                 <div className="top">
+
+//                     <img
+//                         src={currentUser.profilePicture || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHCZuslFbn42wwA9qw6ywBERhtpr_yOFy3Cw&usqp=CAU"}
+//                         alt=""
+//                     />
+
+//                     <input type="text" placeholder={`What's on your mind ${user.name}?`}
+//                         onChange={handleInputChange}
+
+//                     />
+//                 </div>
+//                 <hr />
+//                 <div className="bottom">
+//                     <div className="left">
+//                         <input type="file" id="file" style={{ display: "none" }}
+//                             accept=".png,.jpeg,.jpg,.gif"
+//                             onChange={handleFileChange}
+//                         />
+//                         <label htmlFor="file">
+//                             <div className="item">
+//                                 <img src={Image} alt="" />
+//                                 <span>Add Image</span>
+//                             </div>
+//                         </label>
+//                         <div className="item">
+//                             <img src={Map} alt="Add Place" />
+//                             <span>Add Place</span>
+//                         </div>
+//                         <div className="item">
+//                             <img src={Friend} alt="Tag Friends" />
+//                             <span>Tag Friends</span>
+//                         </div>
+//                     </div>
+//                     <div className="right">
+//                         <button onClick={handleSubmit}>Share</button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Share;
+
+
+
+
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useSessionStorage } from 'react-use';
+import './share.scss';
+import Image from '../../assets/img.png';
+import Map from '../../assets/map.png';
+import Friend from '../../assets/friend.png';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
 
 const Share = () => {
     const { user } = useContext(AuthContext);
-    const currentUser = user.currentUser ;
-   
+    const currentUser = user.currentUser;
 
-    const [desc, setDesc] = useState('')
-    const [selectedFile, setSelectedFile] = useState(null)
+    const [desc, setDesc] = useState('');
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [token, setToken] = useSessionStorage('token');
 
-    const [token, setToken] = useSessionStorage("token");
     const handleInputChange = (e) => {
-        setDesc(e.target.value)
-    }
+        setDesc(e.target.value);
+    };
 
     const handleFileChange = (e) => {
-        setSelectedFile(e.target.files[0])
-    }
+        setSelectedFile(e.target.files[0]);
+    };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const data = new FormData()
-        data.append('desc', desc)
-        data.append('image', selectedFile)
 
-        // try {
-        //     const res = await axios.post('http://localhost:1812/api/posts', data, {
-        //         headers: {
-        //             Authorization: `Bearer ${token}`,
-        //             'Content-Type': 'multipart/form-data'
-        //         }
-        //     })
-        //     alert(res.data)
-        // } catch (err) {
-        //     alert(err.response.data)
-        // }
-        const makePostRequest = async (data) => {
-            const headers = {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('desc: ', desc);
+        console.log('selectedFile: ', selectedFile);
+        const data = new FormData();
+        data.append('img', selectedFile);
+        data.append('desc', desc);
+
+
+        const config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:1812/api/posts',
+            headers: {
                 Authorization: `Bearer ${token}`,
-                'Content-Type': 'multipart/form-data'
-            };
-
-            try {
-                const res = await axios.post('http://localhost:1812/api/posts', data, headers);
-                alert(res.data);
-            } catch (err) {
-                alert(err.response.data);
-            }
+                //Cookie: `refreshToken=${document.cookie}`,
+            },
+            data: data,
         };
-    }
+
+        axios(config)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+
+    //     const data = new FormData();
+    //     data.append('img', selectedFile);
+    //     data.append('desc', desc);
+
+    //     const config = {
+    //         method: 'post',
+    //         maxBodyLength: Infinity,
+    //         url: 'http://localhost:1812/api/posts',
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //             Cookie: `refreshToken=${document.cookie}`,
+    //         },
+    //         data: data,
+    //     };
+
+    //     axios(config)
+    //         .then((response) => {
+    //             console.log(response.data);
+    //         })
+    //         .catch((error) => {
+    //             console.error(error);
+    //         });
+    // };
 
     return (
         <div className="share">
             <div className="container">
                 <div className="top">
-
                     <img
-                        src={currentUser.profilePicture || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHCZuslFbn42wwA9qw6ywBERhtpr_yOFy3Cw&usqp=CAU"}
+                        src={
+                            currentUser.profilePicture ||
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHCZuslFbn42wwA9qw6ywBERhtpr_yOFy3Cw&usqp=CAU'
+                        }
                         alt=""
                     />
-
-                    <input type="text" placeholder={`What's on your mind ${user.name}?`}
+                    <input
+                        type="text"
+                        placeholder={`What's on your mind ${user.name}?`}
                         onChange={handleInputChange}
-
                     />
                 </div>
                 <hr />
                 <div className="bottom">
                     <div className="left">
-                        <input type="file" id="file" style={{ display: "none" }}
+                        <input
+                            type="file"
+                            id="file"
+                            style={{ display: 'none' }}
                             accept=".png,.jpeg,.jpg,.gif"
                             onChange={handleFileChange}
                         />
